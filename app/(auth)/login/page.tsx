54f323319f0ad12c0a7f72f0ae90e54f836b0100
Alchernix <alchernix149@naver.com>
2025-08-01 18:30:28 +0900

    로그인 폼 프론트측 폼 검증 구현

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,44 +1,16 @@
-"use client";
-
-import { useState } from "react";
 import Link from "next/link";
 import { AppleIcon, GoogleIcon } from "@/components/Icons";
 import Container from "@/features/auth/Container";
+import LoginForm from "@/features/auth/LoginForm";
 
 export default function Page() {
-  const [email, setEmail] = useState("");
-  const [pwd, setPwd] = useState("");
-
   return (
     <div className="flex flex-col gap-3">
       <Container classes="h-130">
         <h1 className="w-[362px] text-4xl font-bold text-center text-theme">
           ACHIVA
         </h1>
-        <form className="flex flex-col gap-2.5 w-full">
-          <Input
-            placeholder="이메일 입력"
-            type="email"
-            value={email}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setEmail(e.target.value)
-            }
-          />
-          <Input
-            placeholder="비밀번호 입력"
-            type="password"
-            value={pwd}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setPwd(e.target.value)
-            }
-          />
-          <button
-            className="text-white bg-theme rounded-[5px] px-3 py-1.5 disabled:bg-theme-gray"
-            disabled={!email || !pwd}
-          >
-            로그인
-          </button>
-        </form>
+        <LoginForm />
         <Span>비밀번호 찾기</Span>
         <div className="flex flex-col gap-2">
           <div className="flex items-center gap-2">
@@ -73,13 +45,4 @@ export default function Page() {
 
 function Span({ children }: { children: React.ReactNode }) {
   return <span className="font-light text-sm text-black">{children}</span>;
-}
-
-function Input({ ...props }) {
-  return (
-    <input
-      className="text-sm w-full border border-theme-gray rounded-[5px] px-3 py-1.5 placeholder:text-theme-gray placeholder:font-light"
-      {...props}
-    />
-  );
 }
---
+++ features/auth/LoginForm.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { z } from "zod";
+import { InputHTMLAttributes, useState } from "react";
+import { UserSchema } from "@/features/auth/schima";
+
+export default function LoginForm() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [isEditing, setIsEditing] = useState(false);
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleBlur(field: "email" | "password") {
+    if (field === "email") {
+      const schema = UserSchema.pick({ [field]: true });
+      const payload = { [field]: enteredValues[field] };
+      const result = schema.safeParse(payload);
+      if (!result.success) {
+        const { fieldErrors } = z.flattenError(result.error);
+        setErrors({
+          ...errors,
+          [field]: fieldErrors.email?.[0] || "",
+        });
+      } else {
+        setErrors({
+          ...errors,
+          [field]: "",
+        });
+      }
+    }
+  }
+
+  const isInvalid =
+    !!errors.email || !enteredValues.password || !!errors.password || isEditing;
+
+  return (
+    <form className="flex flex-col gap-2.5 w-full">
+      <Input
+        placeholder="이메일 입력"
+        type="email"
+        value={enteredValues.email}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setIsEditing(true);
+          setEnteredValues((prev) => ({ ...prev, email: e.target.value }));
+        }}
+        onBlur={() => {
+          setIsEditing(false);
+          handleBlur("email");
+        }}
+        error={!isEditing ? errors.email : ""}
+      />
+      <Input
+        placeholder="비밀번호 입력"
+        type="password"
+        value={enteredValues.password}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setIsEditing(true);
+          setEnteredValues((prev) => ({ ...prev, password: e.target.value }));
+        }}
+        onBlur={() => {
+          setIsEditing(false);
+          handleBlur("password");
+        }}
+        error={!isEditing ? errors.password : ""}
+      />
+      <button
+        className="text-white bg-theme rounded-[5px] px-3 py-1.5 disabled:bg-theme-gray"
+        disabled={isInvalid}
+      >
+        로그인
+      </button>
+    </form>
+  );
+}
+
+type InputProps = {
+  error: string;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+function Input({ error, ...props }: InputProps) {
+  return (
+    <div>
+      <input
+        className={`text-sm w-full border ${
+          error ? "border-theme-red" : "border-theme-gray"
+        } rounded-[5px] px-3 py-1.5 placeholder:text-theme-gray placeholder:font-light`}
+        {...props}
+      />
+      <p className="mt-0.5 text-theme-red text-xs font-light">{error}</p>
+    </div>
+  );
+}
