d67eecbe5c200cda196db3ccda8f4d419334f230
Alchernix <alchernix149@naver.com>
2025-08-22 19:19:01 +0900

    야매최적화22

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -13,3 +13,17 @@ export async function POST() {
   });
   return res;
 }
+
+export async function GET() {
+  const res = NextResponse.json({ success: true });
+  res.cookies.set({
+    name: "token",
+    value: "",
+    path: "/",
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    expires: new Date(0), // 즉시 만료
+  });
+  return res;
+}
