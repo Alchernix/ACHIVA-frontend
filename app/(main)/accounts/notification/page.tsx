f2ad657a6957d5b62b6712985e022a159c5509cf
Alchernix <alchernix149@naver.com>
2025-09-02 14:10:16 +0900

    응원함기능 구현중

--- app/(main)/accounts/notification/page.tsx
+++ app/(main)/accounts/notification/page.tsx
@@ -3,9 +3,9 @@ import Footer from "@/components/Footer";
 
 export default function Page() {
   return (
-    <div className="w-full flex justify-center">
-      <div className="w-md">
-        <h2 className="font-semibold text-xl text-theme my-5 w-full text-left">
+    <div className="flex-1 w-full flex justify-center">
+      <div className="flex flex-col w-md">
+        <h2 className="font-semibold text-2xl text-theme mt-5 w-full text-left">
           응원함
         </h2>
         <Notifications />
--- features/user/Notifications.tsx
+++ features/user/Notifications.tsx
@@ -2,7 +2,8 @@
 
 import { LoadingIcon } from "@/components/Icons";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
+import type { PostRes } from "@/types/Post";
 import type { NotificationsRes } from "@/types/responses";
 import ProfileImg from "@/components/ProfileImg";
 import dateFormatter from "@/lib/dateFormatter";
@@ -14,13 +15,12 @@ import {
 } from "@/components/Icons";
 import Link from "next/link";
 
-type ProfileImgCacheType = { [id: number]: string };
-
-const userCache = new Set<number>();
+// 유저별 프로필 이미지(임시)
+const userCache = new Map<number, string | undefined>();
+// 포스트 캐시
+const postCache = new Map<number, PostRes | undefined>();
 
 export default function Notifications() {
-  const [profileImgs, setProfileImgs] = useState<ProfileImgCacheType>({});
-
   const icons = {
     최고예요: ThumbUpCheerIcon,
     수고했어요: FireCheerIcon,
@@ -44,16 +44,27 @@ export default function Notifications() {
     await Promise.all(
       json.data.content.map(async (notification: any) => {
         if (!userCache.has(notification.senderId)) {
-          userCache.add(notification.senderId);
+          userCache.set(notification.senderId, undefined);
           const userRes = await fetch(
             `/api/members?memberId=${notification.senderId}`
           );
           const userJson = await userRes.json();
-          setProfileImgs((prev) => ({
-            ...prev,
-            [notification.senderId]: userJson.data.profileImageUrl,
-          }));
+          userCache.set(notification.senderId, userJson.data.profileImageUrl);
           return userJson;
+        }
+      })
+    );
+
+    await Promise.all(
+      json.data.content.map(async (notification: any) => {
+        if (!postCache.has(notification.articleId)) {
+          postCache.set(notification.articleId, undefined);
+          const postRes = await fetch(
+            `/api/posts?postId=${notification.articleId}`
+          );
+          const postJson = await postRes.json();
+          postCache.set(notification.articleId, postJson.data);
+          return postJson;
         }
       })
     );
@@ -92,37 +103,79 @@ export default function Notifications() {
   const notifications = data?.pages.flatMap((p) => p.content) ?? [];
 
   return (
-    <>
+    <div className="flex-1">
       {isLoading && (
-        <div className="w-full flex my-2 justify-center">
-          <LoadingIcon color="text-theme" />
+        <div className="mt-5 divide-y divide-black/15">
+          {Array(10)
+            .fill(0)
+            .map((_, idx) => (
+              <NotificationSkeleton key={idx} />
+            ))}
         </div>
-        // <div className="w-full flex justify-center">
-        //   <LoadingIcon color="text-theme" />
-        // </div>
       )}
-      {/* {posts.length === 0 && !isLoading && <HomePost post={getFirstPage()} />} */}
-      <ul className="w-full flex flex-col gap-7">
-        {notifications.map((n) => {
+      {notifications.length === 0 && !isLoading && (
+        <p className="h-full flex justify-center items-center text-[#808080]">
+          아직 받은 응원이 없어요.
+        </p>
+      )}
+      <ul className="w-full flex flex-col">
+        {notifications.map((n, idx) => {
           const Icon = icons[n.cheeringCategory];
+          const hasTitle =
+            idx === 0 ||
+            notifications[idx - 1].articleId !== notifications[idx].articleId;
           return (
-            <li key={n.id} className="flex gap-2.5 items-center">
-              <Link href={`/${n.senderName}`}>
-                <ProfileImg url={profileImgs[n.senderId]} size={50} />
-              </Link>
-              <div className="flex-1 flex gap-2.5 items-center">
-                <Link href={`/${n.senderName}`} className="font-semibold">
-                  {n.senderName}
+            <div key={n.id}>
+              {hasTitle && (
+                <Link
+                  href={`/post/${n.articleId}`}
+                  className="flex items-center gap-3 mt-5 mb-3"
+                >
+                  <div className="font-semibold text-xl bg-theme text-white rounded-sm px-4 py-1.5">
+                    {postCache.get(n.articleId)?.category}
+                  </div>
+                  <p className="text-[#808080] font-light">
+                    {postCache.get(n.articleId)?.authorCategorySeq}번째 이야기
+                  </p>
+                  <button className="ml-auto">
+                    <svg
+                      width="25"
+                      height="26"
+                      viewBox="0 0 25 26"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M8.82165 5.74029C8.74907 5.6677 8.69149 5.58153 8.6522 5.48669C8.61292 5.39185 8.5927 5.2902 8.5927 5.18755C8.5927 5.0849 8.61292 4.98325 8.6522 4.88841C8.69149 4.79358 8.74907 4.7074 8.82165 4.63482C8.89424 4.56223 8.98041 4.50465 9.07525 4.46537C9.17009 4.42609 9.27173 4.40587 9.37439 4.40587C9.47704 4.40587 9.57869 4.42609 9.67352 4.46537C9.76836 4.50465 9.85453 4.56223 9.92712 4.63482L17.7396 12.4473C17.8123 12.5199 17.8699 12.606 17.9092 12.7009C17.9485 12.7957 17.9688 12.8974 17.9688 13.0001C17.9688 13.1027 17.9485 13.2044 17.9092 13.2992C17.8699 13.3941 17.8123 13.4802 17.7396 13.5528L9.92712 21.3653C9.78053 21.5119 9.5817 21.5942 9.37439 21.5942C9.16707 21.5942 8.96825 21.5119 8.82165 21.3653C8.67506 21.2187 8.5927 21.0199 8.5927 20.8126C8.5927 20.6052 8.67506 20.4064 8.82165 20.2598L16.0824 13.0001L8.82165 5.74029Z"
+                        fill="#B3B3B3"
+                      />
+                    </svg>
+                  </button>
                 </Link>
-                <p className="font-light text-black/50">
-                  {dateFormatter(n.createdAt)}
-                </p>
-                <div className="ml-auto text-[15px] sm:text-base flex items-center gap-[2px] sm:gap-1 rounded-full border border-theme px-1.5 sm:px-3 py-1 text-white bg-theme">
-                  <p>{n.cheeringCategory}</p>
-                  <Icon active />
+              )}
+
+              <li
+                className={`flex gap-2.5 items-center py-2 ${
+                  hasTitle ? "" : "border-t border-t-black/15"
+                }`}
+              >
+                <Link href={`/${n.senderName}`}>
+                  <ProfileImg url={userCache.get(n.senderId)!} size={50} />
+                </Link>
+                <div className="flex-1 flex gap-2.5 items-center">
+                  <Link href={`/${n.senderName}`} className="font-semibold">
+                    {n.senderName}
+                  </Link>
+                  <p className="font-light text-black/50">
+                    {dateFormatter(n.createdAt)}
+                  </p>
+                  <div className="ml-auto text-[15px] sm:text-base flex items-center gap-[2px] sm:gap-1 rounded-full border border-theme px-1.5 sm:px-3 py-1 text-white bg-theme">
+                    <p>{n.cheeringCategory}</p>
+                    <Icon active />
+                  </div>
                 </div>
-              </div>
-            </li>
+              </li>
+            </div>
           );
         })}
       </ul>
@@ -132,6 +185,24 @@ export default function Notifications() {
           <LoadingIcon color="text-theme" />
         </div>
       )}
-    </>
+    </div>
+  );
+}
+
+function NotificationSkeleton() {
+  return (
+    <li className={`flex gap-2.5 items-center py-2`}>
+      <div className="w-[50px] rounded-full aspect-square bg-loading animate-pulse" />
+      <div className="flex-1 flex gap-2.5 items-center">
+        <div className="h-6 w-40 bg-loading animate-pulse rounded-md" />
+        {/* <p className="font-light text-black/50">
+                    {dateFormatter(n.createdAt)}
+                  </p> */}
+        {/* <div className="ml-auto text-[15px] sm:text-base flex items-center gap-[2px] sm:gap-1 rounded-full border border-theme px-1.5 sm:px-3 py-1 text-white bg-theme">
+                    <p>{n.cheeringCategory}</p>
+                    <Icon active />
+                  </div> */}
+      </div>
+    </li>
   );
 }
