9b31670a8f73563f103537d9172bbec6b423ebe1
Alchernix <alchernix149@naver.com>
2025-09-03 14:51:12 +0900

    drawer 구조 변경

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,27 +1,22 @@
 // 사이드바에서 열리는 drawer
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
 import { motion } from "motion/react";
-import { useEffect } from "react";
-import { AnimatePresence } from "motion/react";
+import { useEffect, useRef } from "react";
 // import { createPortal } from "react-dom";
 
 type ModalProps = {
   title?: React.ReactNode;
-  path: string;
   children: React.ReactNode;
-  onClose?: (() => void) | undefined;
+  onClose: () => void;
 };
 
 export default function Drawer({
   title = null,
-  path,
   children,
-  onClose = undefined,
+  onClose,
 }: ModalProps) {
-  const pathname = usePathname();
-  const isOpen = pathname === path;
+  const drawerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     // 현재 스크롤바 너비 계산
@@ -35,50 +30,47 @@ export default function Drawer({
     document.body.style.overflow = "hidden";
     if (sbw > 0) document.body.style.paddingRight = `${sbw}px`;
 
-    if (!isOpen) {
-      document.body.style.overflow = prevOverflow;
-      document.body.style.paddingRight = prevPaddingRight;
+    function handleClickOutSide(e: MouseEvent) {
+      const target = e.target as HTMLElement;
+      if (target.closest("a") || !drawerRef.current?.contains(target)) {
+        onClose();
+      }
     }
 
+    document.addEventListener("click", handleClickOutSide);
     return () => {
       document.body.style.overflow = prevOverflow;
       document.body.style.paddingRight = prevPaddingRight;
+      document.removeEventListener("click", handleClickOutSide);
     };
-  }, [isOpen]);
+  }, [onClose]);
 
-  const router = useRouter();
   return (
-    <AnimatePresence>
-      {isOpen && (
-        <div
-          onClick={router.back}
-          className="fixed left-21 h-dvh top-0 w-full z-10 flex justify-start bg-black/50 overflow-hidden"
-        >
-          <motion.div
-            onClick={(e) => e.stopPropagation()}
-            onWheel={(e) => e.stopPropagation()}
-            initial={{ x: -448, opacity: 0.5 }}
-            animate={{ x: 0, opacity: 1 }}
-            exit={{ x: -448, opacity: 0.5 }}
-            transition={{
-              duration: 0.3,
-              ease: "easeOut",
-            }}
-            style={{
-              boxShadow:
-                "111px 0 31px 0 rgba(0, 0, 0, 0.00), 71px 0 28px 0 rgba(0, 0, 0, 0.01), 40px 0 24px 0 rgba(0, 0, 0, 0.03), 18px 0 18px 0 rgba(0, 0, 0, 0.04), 4px 0 10px 0 rgba(0, 0, 0, 0.05)",
-            }}
-            className="rounded-r-lg bg-white overflow-hidden w-md"
-          >
-            <div className="flex flex-col h-full p-5 overflow-auto">
-              <div className="font-bold text-2xl text-theme mb-5 w-full text-left">
-                {title}
-              </div>
-              <div className="flex-1">{children}</div>
-            </div>
-          </motion.div>
+    <div className="fixed left-21 h-dvh top-0 w-full z-50 flex justify-start bg-black/50 overflow-hidden">
+      <motion.div
+        ref={drawerRef}
+        onClick={(e) => e.stopPropagation()}
+        onWheel={(e) => e.stopPropagation()}
+        initial={{ x: -448, opacity: 0.5 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: -448, opacity: 0.5 }}
+        transition={{
+          duration: 0.3,
+          ease: "easeOut",
+        }}
+        style={{
+          boxShadow:
+            "111px 0 31px 0 rgba(0, 0, 0, 0.00), 71px 0 28px 0 rgba(0, 0, 0, 0.01), 40px 0 24px 0 rgba(0, 0, 0, 0.03), 18px 0 18px 0 rgba(0, 0, 0, 0.04), 4px 0 10px 0 rgba(0, 0, 0, 0.05)",
+        }}
+        className="rounded-r-lg bg-white overflow-hidden w-md"
+      >
+        <div className="flex flex-col h-full p-5 overflow-auto">
+          <div className="font-bold text-2xl text-theme mb-5 w-full text-left">
+            {title}
+          </div>
+          <div className="flex-1">{children}</div>
         </div>
-      )}
-    </AnimatePresence>
+      </motion.div>
+    </div>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,103 +10,126 @@ import {
   NotificationIcon,
   SidebarSettingIcon,
 } from "./Icons";
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import ProfileImg from "./ProfileImg";
+import { useState } from "react";
+import Drawer from "./Drawer";
+import Notifications from "@/features/user/Notifications";
 
 export default function Sidebar({ user }: { user: User }) {
+  const [openedDrawer, setOpenedDrawer] = useState<"검색" | "응원함" | null>(
+    null
+  );
+
+  const drawerContent =
+    openedDrawer === "검색" ? (
+      <Drawer title="검색" onClose={() => setOpenedDrawer(null)}>
+        <div className="w-full h-full flex items-center justify-center text-[#808080]">
+          아직 준비중인 기능이에요.
+        </div>
+      </Drawer>
+    ) : (
+      <Drawer title="응원함" onClose={() => setOpenedDrawer(null)}>
+        <Notifications />
+      </Drawer>
+    );
+
   const pathname = usePathname();
   let selected;
-  if (pathname === "/") {
+  if (openedDrawer === "응원함") {
+    selected = "응원함";
+  } else if (openedDrawer === "검색") {
+    selected = "검색";
+  } else if (pathname === "/") {
     selected = "홈";
   } else if (pathname.startsWith("/settings")) {
     selected = "설정";
   } else if (pathname === "/post/create") {
     selected = "글쓰기";
-  } else if (pathname === "/accounts/notification") {
-    selected = "응원함";
   } else {
     selected = "프로필";
   }
 
-  const isNavFolded = pathname === "/accounts/notification";
-
   return (
-    <motion.nav
-      layoutScroll
-      className={`text-theme z-10 h-dvh fixed bottom-0 top-0 flex flex-col items-center w-auto lg:w-60 ${
-        isNavFolded ? "!w-auto" : ""
-      } py-8 border-r border-r-[#d9d9d9] bg-white`}
-    >
-      <div
-        className={`mb-15 w-full h-[39.29px] flex px-6 justify-start lg:hidden ${
-          isNavFolded ? "!block" : ""
-        }`}
+    <>
+      <motion.nav
+        layoutScroll
+        className={`text-theme z-10 h-dvh fixed bottom-0 top-0 flex flex-col items-center w-auto lg:w-60 ${
+          openedDrawer ? "!w-auto" : ""
+        } py-8 border-r border-r-[#d9d9d9] bg-white`}
       >
-        <Link href="/" className="h-full flex items-end">
-          <Logo />
-        </Link>
-      </div>
-      <div
-        className={`mb-15 w-full justify-start px-6 hidden lg:flex ${
-          isNavFolded ? "!hidden" : ""
-        }`}
-      >
-        <Link href="/">
-          <TextLogo />
-        </Link>
-      </div>
-      <ul className="flex-1 flex flex-col w-full justify-around gap-5">
-        <Link href="/">
-          <ListItem
-            isNavFolded={isNavFolded}
-            label="홈"
-            icon={<HomeIcon fill={selected === "홈"} />}
-            selected={selected === "홈"}
-          />
-        </Link>
-        <Link href="/search" scroll={false}>
-          <ListItem
-            isNavFolded={isNavFolded}
-            label="검색"
-            icon={<SearchIcon fill={selected === "검색"} />}
-            selected={selected === "검색"}
-          />
-        </Link>
-        <Link href="/post/create" scroll={false}>
-          <ListItem
-            isNavFolded={isNavFolded}
-            label="글쓰기"
-            icon={<PostIcon fill={selected === "글쓰기"} />}
-            selected={selected === "글쓰기"}
-          />
-        </Link>
-        <Link href="/accounts/notification" scroll={false}>
-          <ListItem
-            isNavFolded={isNavFolded}
-            label="응원함"
-            icon={<NotificationIcon fill={selected === "응원함"} />}
-            selected={selected === "응원함"}
-          />
-        </Link>
-        <Link href={`/${user.nickName}`}>
-          <ListItem
-            isNavFolded={isNavFolded}
-            label="프로필"
-            icon={<ProfileImg size={32} url={user.profileImageUrl} />}
-            selected={selected === "프로필"}
-          />
-        </Link>
+        <div
+          className={`mb-15 w-full h-[39.29px] flex px-6 justify-start lg:hidden ${
+            openedDrawer ? "!block" : ""
+          }`}
+        >
+          <Link href="/" className="h-full flex items-end">
+            <Logo />
+          </Link>
+        </div>
+        <div
+          className={`mb-15 w-full justify-start px-6 hidden lg:flex ${
+            openedDrawer ? "!hidden" : ""
+          }`}
+        >
+          <Link href="/">
+            <TextLogo />
+          </Link>
+        </div>
+        <ul className="flex-1 flex flex-col w-full justify-around gap-5">
+          <Link href="/">
+            <ListItem
+              isNavFolded={!!openedDrawer}
+              label="홈"
+              icon={<HomeIcon fill={selected === "홈"} />}
+              selected={selected === "홈"}
+            />
+          </Link>
+          <button onClick={() => setOpenedDrawer("검색")}>
+            <ListItem
+              isNavFolded={!!openedDrawer}
+              label="검색"
+              icon={<SearchIcon fill={selected === "검색"} />}
+              selected={selected === "검색"}
+            />
+          </button>
+          <Link href="/post/create" scroll={false}>
+            <ListItem
+              isNavFolded={!!openedDrawer}
+              label="글쓰기"
+              icon={<PostIcon fill={selected === "글쓰기"} />}
+              selected={selected === "글쓰기"}
+            />
+          </Link>
+          <button onClick={() => setOpenedDrawer("응원함")}>
+            <ListItem
+              isNavFolded={!!openedDrawer}
+              label="응원함"
+              icon={<NotificationIcon fill={selected === "응원함"} />}
+              selected={selected === "응원함"}
+            />
+          </button>
+          <Link href={`/${user.nickName}`}>
+            <ListItem
+              isNavFolded={!!openedDrawer}
+              label="프로필"
+              icon={<ProfileImg size={32} url={user.profileImageUrl} />}
+              selected={selected === "프로필"}
+            />
+          </Link>
 
-        <Link href={`/settings/accounts/password`} className="mt-auto">
-          <ListItem
-            isNavFolded={isNavFolded}
-            label="설정"
-            icon={<SidebarSettingIcon fill={selected === "설정"} />}
-            selected={selected === "설정"}
-          />
-        </Link>
-      </ul>
-    </motion.nav>
+          <Link href={`/settings/accounts/password`} className="mt-auto">
+            <ListItem
+              isNavFolded={!!openedDrawer}
+              label="설정"
+              icon={<SidebarSettingIcon fill={selected === "설정"} />}
+              selected={selected === "설정"}
+            />
+          </Link>
+        </ul>
+      </motion.nav>
+      <AnimatePresence>{openedDrawer && drawerContent}</AnimatePresence>
+    </>
   );
 }
 
